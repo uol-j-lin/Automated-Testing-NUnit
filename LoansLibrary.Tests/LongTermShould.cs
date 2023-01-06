40ea30a838d17797a5e7f4c5c6eba807724d125a
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-06 14:50:28 -0500

    Add unit test for ToMonths method of LoanTerm class (using TestFixture and Test NUnit attributes)

---
+++ LoansLibrary.Tests/LongTermShould.cs
@@ -0,0 +1,23 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoansLibrary.Tests
+{
+    [TestFixture]
+    internal class LongTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            // system under test
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+    }
+}
