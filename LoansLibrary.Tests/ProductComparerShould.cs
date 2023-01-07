e924bf5c23007f264630743db508bc6add1a1a1b
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-07 14:21:47 -0500

    Assert on a collection of MonthlyRepaymentComparison objects for number of items after using the CompareMonthlyRepayments method

---
+++ LoansLibrary.Tests/ProductComparerShould.cs
@@ -0,0 +1,30 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoansLibrary.Tests
+{
+    public class ProductComparerShould
+    {
+        [Test]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3),
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Has.Exactly(3).Items);
+        }
+    }
+}
