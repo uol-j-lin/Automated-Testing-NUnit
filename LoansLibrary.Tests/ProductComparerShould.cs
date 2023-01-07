53f576e8269eb48f5707bccd54e4ad4ad93db9bf
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-07 14:40:18 -0500

    Add test for presence of product, without expected monthly repayment

--- LoansLibrary.Tests/ProductComparerShould.cs
+++ LoansLibrary.Tests/ProductComparerShould.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace LoansLibrary.Tests
 {
+    [TestFixture]
     public class ProductComparerShould
     {
         [Test]
@@ -62,6 +63,26 @@ namespace LoansLibrary.Tests
             var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
 
             Assert.That(comparisons, Does.Contain(expectedProduct));
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct_WithPartialKnownExpectedValues()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3),
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+
+            // don't care about the expected monthly repayment, only that the product is there
+            Assert.That(comparisons, Has.Exactly(1).Property("ProductName").EqualTo("a").And.Property("InterestRate").EqualTo(1).And.Property("MonthlyRepayment").GreaterThan(0));
         }
     }
 }
