edb63bb9f393ffddb15fb44a4add1303a02259ee
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-08 10:31:09 -0500

    Add two tests for equality/inequality for MonthlyRepaymentComparison class

--- LoansLibrary.Tests/MonthlyRepaymentComparisonShould.cs
+++ LoansLibrary.Tests/MonthlyRepaymentComparisonShould.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Loans.Domain.Applications;
+using NUnit.Framework;
 
 namespace LoansLibrary.Tests
 {
-    internal class MonthlyRepaymentComparisonShould
+    [TestFixture]
+    public class MonthlyRepaymentComparisonShould
     {
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = new MonthlyRepaymentComparison("a", 42.42m, 22.22m);
+            var b = new MonthlyRepaymentComparison("a", 42.42m, 22.22m);
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void RespectValueInequality()
+        {
+            var a = new MonthlyRepaymentComparison("a", 42.42m, 22.22m);
+            var b = new MonthlyRepaymentComparison("a", 42.42m, 23.22m);
+
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
     }
 }
