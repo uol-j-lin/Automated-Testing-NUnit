4be003942e844e036624411e25413d4b2e9f5c7b
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-09 06:50:25 -0500

    Add test for calculate monthly payment method with hardcoded loan amount

--- LoansLibrary.Tests/LaonRepaymentCalculatorShould.cs
+++ LoansLibrary.Tests/LaonRepaymentCalculatorShould.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Loans.Domain.Applications;
 using NUnit.Framework;
 
 namespace LoansLibrary.Tests
@@ -11,6 +12,13 @@ namespace LoansLibrary.Tests
     public class LaonRepaymentCalculatorShould
 
     {
+        [Test]
+        public void CalculateCorrectMonthlyRepayment()
+        {
+            var sut = new LoanRepaymentCalculator();
+            var monthlyPayment = sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
 
+            Assert.That(monthlyPayment, Is.EqualTo(1264.14));
+        }
     }
 }
