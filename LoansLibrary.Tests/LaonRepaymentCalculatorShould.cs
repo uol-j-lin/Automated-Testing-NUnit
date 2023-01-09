383fc8e50e99ab69856554fcc3d72b3a7fec4d4e
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-09 06:48:02 -0500

    Add test fixture for monthly repayment

---
+++ LoansLibrary.Tests/LaonRepaymentCalculatorShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LoansLibrary.Tests
+{
+    [TestFixture]
+    public class LaonRepaymentCalculatorShould
+
+    {
+
+    }
+}
