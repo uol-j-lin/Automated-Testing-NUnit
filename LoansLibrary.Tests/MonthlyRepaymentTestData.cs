163b119ecfc181d4003c33b2477f7dece852103a
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-11 07:44:19 -0500

    Add another test case in the centralized test source

--- LoansLibrary.Tests/MonthlyRepaymentTestData.cs
+++ LoansLibrary.Tests/MonthlyRepaymentTestData.cs
@@ -16,6 +16,7 @@ namespace LoansLibrary.Tests
             {
                 yield return new TestCaseData(200_000m, 6.5m, 30, 1264.14m);
                 yield return new TestCaseData(500_000m, 10m, 30, 4387.86m);
+                yield return new TestCaseData(200_000m, 10m, 30, 1755.14m);
             }
         }
     }
