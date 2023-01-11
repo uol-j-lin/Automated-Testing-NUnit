7204c649f39e9bea6675413ea3d73b19e003e3eb
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-10 20:53:41 -0500

    Add test class for CalculateMonthlyRepayment method

---
+++ LoansLibrary.Tests/LoanRepaymentCalculatorShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loans.Domain.Applications;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace LoansLibrary.Tests
+{
+    public class LoanRepaymentCalculatorShould
+    {
+        [Test]
+        public void CalculateCorrectMonthlyRepayment()
+        {
+            var sut = new LoanRepaymentCalculator();
+            var monthlyPayment = sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
+
+            Assert.That(monthlyPayment, Is.EqualTo(1264.14));
+        }
+    }
+}
