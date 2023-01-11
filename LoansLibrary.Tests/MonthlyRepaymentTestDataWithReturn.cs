2937ad27110b60e3ef739bded26517710813e3cf
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-11 13:19:38 -0500

    Create centralized monthly repayment test data, specifying the expected return value outside the constructor

---
+++ LoansLibrary.Tests/MonthlyRepaymentTestDataWithReturn.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoansLibrary.Tests
+{
+    internal class MonthlyRepaymentTestDataWithReturn
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(200_000m, 6.5m, 30).Returns(1264.14);
+                yield return new TestCaseData(500_000m, 10m, 30).Returns(4387.86m);
+                yield return new TestCaseData(200_000m, 10m, 30).Returns(1755.14m);
+            }
+        }
+    }
+}
