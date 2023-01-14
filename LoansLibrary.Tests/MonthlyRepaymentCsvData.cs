afc8f3984f1ad57592ccae9d355720c9a63468a7
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-14 18:15:29 -0500

    Extract data from csv file for test case data

--- LoansLibrary.Tests/MonthlyRepaymentCsvData.cs
+++ LoansLibrary.Tests/MonthlyRepaymentCsvData.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace LoansLibrary.Tests
+namespace Loans.Tests
 {
-    internal class MonthlyRepaymentCsvData
+    public class MonthlyRepaymentCsvData
     {
+        public static IEnumerable GetTestCases(string csvFileName)
+        {
+            var csvLines = File.ReadAllLines(csvFileName);
+
+            var testCases = new List<TestCaseData>();
+
+            foreach (var line in csvLines)
+            {
+                string[] values = line.Replace(" ", "").Split(',');
+
+                decimal principal = decimal.Parse(values[0]);
+                decimal interestRate = decimal.Parse(values[1]);
+                int termInYears = int.Parse(values[2]);
+                decimal expectedRepayment = decimal.Parse(values[3]);
+
+                testCases.Add(new TestCaseData(principal, interestRate, termInYears, expectedRepayment));
+            }
+
+            return testCases;
+        }
     }
 }
