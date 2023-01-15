bd8627ba317a64b79cdfff4147a593cde7f4d3df
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-15 10:13:01 -0500

    Create a product comparison attribute to use across methods or classes

---
+++ LoansLibrary.Tests/ProductComparisonAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LoansLibrary.Tests
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    class ProductComparisonAttribute : CategoryAttribute
+    {
+    }
+}
